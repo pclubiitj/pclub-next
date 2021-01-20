79d54533e8424ac5f6d7717403164dd72dbce77e
kunaltawatia <tawatia.1@iitj.ac.in>
2021-01-20 22:25:28 +0530

    prettier formatting; font initialization

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,13 +2,11 @@ import Nav from "../components/nav";
 
 export default function IndexPage() {
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
+    <div className="py-20 text-center space-y-4">
+      <h1 className="text-5xl font-bold text-gray-800">Programming Club</h1>
+      <h3 className="text-2xl font-mono text-gray-500">
+        Indian Institute of Technology, Jodhpur
+      </h3>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,9 @@ module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media",
   theme: {
-    extend: {
-      colors: {
-        "accent-1": "#333",
-      },
+    fontFamily: {
+      sans: ["'Poppins'"],
+      mono: ["'Source Code Pro'"],
     },
   },
   variants: {
