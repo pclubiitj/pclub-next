d4860a50549d882bd46ede8dad7ae32739b199df
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2021-01-25 17:52:45 +0530

    add lint to pre commit hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/index.css';
 import Nav from '../components/Navbar';
+import PropTypes from 'prop-types';
 
 function App({ Component, pageProps }) {
+    console.log(pageProps);
     return (
         <div>
             <Nav />
@@ -9,5 +11,10 @@ function App({ Component, pageProps }) {
         </div>
     );
 }
+
+App.propTypes = {
+    Component: PropTypes.any,
+    pageProps: PropTypes.object
+};
 
 export default App;
