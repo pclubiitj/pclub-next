1ca94a7c33f14f176c8e7b6a09586394170a2bf9
kunaltawatia <tawatia.1@iitj.ac.in>
2021-01-29 17:21:32 +0530

    Added discord embed

---
+++ components/Discord.js
@@ -0,0 +1,19 @@
+import { bool } from "prop-types";
+
+export default function Discord({ dark }) {
+	return (
+		<iframe
+			src={`https://discord.com/widget?id=776838119877115946&theme=${dark ? "dark" : "light"}`}
+			width="350"
+			height="500"
+			allowtransparency="true"
+			frameBorder="0"
+			sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+			title="discord"
+			className="shadow-md p-4 rounded-lg"
+		/>
+	);
+}
+Discord.propTypes = {
+	dark: bool,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import { motion } from "framer-motion";
+import Discord from "../components/Discord";
 
 export default function IndexPage() {
 	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ duration: 1 }}
-			className="py-20 text-center space-y-4 h-full"
-		>
-			<h1 className="text-5xl font-bold text-gray-800">Programming Club</h1>
-			<h3 className="text-2xl font-mono text-gray-500">Indian Institute of Technology, Jodhpur</h3>
-		</motion.div>
+		<div className="flex flex-col items-center">
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1 }}
+				className="py-20 text-center space-y-4 h-full"
+			>
+				<h1 className="text-5xl font-bold text-gray-800">Programming Club</h1>
+				<h3 className="text-2xl font-mono text-gray-500">Indian Institute of Technology, Jodhpur</h3>
+			</motion.div>
+			<Discord dark={false} />
+		</div>
 	);
 }
